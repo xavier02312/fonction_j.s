31a802ac1db9d16b01b304a57a869444535fd46f
xavier02312 <paqueriaudxavier@gmail.com>
2022-04-05 13:33:14 +0200

    continu

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * LES FONCTIONS
+ */
+
+function nomDeLaFonction() {
+    //...
+}
+
+let nomDeFonction = function() {
+    console.log("Tu viens de m'appeler");
+}
+
+let nomFonction = () => {
+    //...
+}
+
+nomDeFonction();
